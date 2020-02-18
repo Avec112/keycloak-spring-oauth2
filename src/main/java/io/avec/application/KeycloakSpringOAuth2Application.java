953fcb685cbe9b35252aeccc5c9cd118c8eefd04
Avec <avec112@gmail.com>
2020-02-18 18:10:55 +0100

    Initial commit

---
+++ src/main/java/io/avec/application/KeycloakSpringOAuth2Application.java
@@ -0,0 +1,19 @@
+package io.avec.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
+        UserDetailsServiceAutoConfiguration.class})
+public class KeycloakSpringOAuth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakSpringOAuth2Application.class, args);
+    }
+
+
+}
---
+++ src/test/java/io/avec/outputtracker/OutputTrackerApplicationTests.java
@@ -0,0 +1,15 @@
+package io.avec.outputtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//import org.junit.Test;
+
+@SpringBootTest
+class OutputTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
